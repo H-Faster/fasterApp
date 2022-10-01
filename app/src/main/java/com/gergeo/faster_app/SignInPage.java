57c35b4f8b29bb4d7be51082dd3a53552754d953
gerkoff <adedihagermain@gmail.com>
2022-10-01 17:29:22 +0300

    designed first pages

---
+++ app/src/main/java/com/gergeo/faster_app/SignInPage.java
@@ -0,0 +1,14 @@
+package com.gergeo.faster_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
