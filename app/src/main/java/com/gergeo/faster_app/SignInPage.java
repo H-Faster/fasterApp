c5a3d51ae8cb2403cf52edb10b8866522125a1cf
gerkoff <adedihagermain@gmail.com>
2022-10-15 19:43:39 +0300

    Sign In page done

--- app/src/main/java/com/gergeo/faster_app/SignInPage.java
+++ app/src/main/java/com/gergeo/faster_app/SignInPage.java
@@ -2,13 +2,54 @@ package com.gergeo.faster_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignInPage extends AppCompatActivity {
+
+    EditText username , password;
+    Button btnlogin;
+    DBHelper dbHelper;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        dbHelper = new DBHelper(this);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        btnlogin = (Button) findViewById(R.id.loginbutton);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if (user.equals("")||pass.equals(""))
+                {
+                    Toast.makeText(SignInPage.this, "Please enter your credentials", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Boolean checkuserpass = dbHelper.CheckUserNamePassword(user,pass);
+                    if (checkuserpass == true){
+                        Toast.makeText(SignInPage.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                        //Intent intent = new Intent(getApplicationContext(), CLASSES POUR LES OPTIONS);
+                        //startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(SignInPage.this, "SInvalid credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
 }
